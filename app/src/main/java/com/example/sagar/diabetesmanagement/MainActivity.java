1eb763bd0941ae86ab29eee43027ace0489b6578
Sagar <patel8@uwm.edu>
2016-07-10 02:19:44 -0500

    Added main screen

--- app/src/main/java/com/example/sagar/diabetesmanagement/MainActivity.java
+++ app/src/main/java/com/example/sagar/diabetesmanagement/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.sagar.diabetesmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.enter_info);
     }
+
+
+
+
+    // This method will add main_menu.xml to this activity.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+
+        return true;
+    }
+
+
+
 }
