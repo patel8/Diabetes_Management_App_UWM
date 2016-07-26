ebb548cab1cf6eadbdddbe800b1ca928b452b760
Sagar <patel8@uwm.edu>
2016-07-26 18:41:09 -0500

    Added Two screens.

---
+++ app/src/main/java/com/example/sagar/diabetesmanagement/home_activity.java
@@ -0,0 +1,129 @@
+package com.example.sagar.diabetesmanagement;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class home_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+        refreshAverageGlucoceValues();
+
+    }
+
+    // This method will add main_menu.xml to this activity.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+
+        return true;
+    }
+
+    //This method will perform action when 'Add Activity button' is called.
+
+    public void onButtonAddActivityClick(View view){
+
+        final CharSequence[] items = {" Diet "," BGL "," Exercise "," Other "};
+// arraylist to keep the selected items
+        final ArrayList seletedItems =new ArrayList();
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Select an Activity")
+                .setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                        if (isChecked) {
+                            // If the user checked the item, add it to the selected items
+                            seletedItems.add(indexSelected);
+                        } else if (seletedItems.contains(indexSelected)) {
+                            // Else, if the item is already in the array, remove it
+                            seletedItems.remove(Integer.valueOf(indexSelected));
+                        }
+                    }
+                }).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Your code when user clicked on OK
+                        //  You can write the code  to save the selected item here
+
+
+
+                            Intent intent = new Intent(getApplicationContext(), exercise_activity.class);
+                            startActivity(intent);
+
+
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Your code when user clicked on Cancel
+                    }
+                }).create();
+        dialog.show();
+
+
+    }
+
+
+
+    public void refreshAverageGlucoceValues()
+    {
+        setDailyGlucoceText();
+        setMonthlyGlucoceText();
+        setWeeklyGlucoceText();
+    }
+
+
+    private void setDailyGlucoceText()
+    {
+
+        //Perform Database Operations in orderto get Daily Glucoze
+
+        TextView DailyTextView = (TextView) findViewById(R.id.txtDaily);
+        //Set the Text for Daily Text View
+
+
+
+    }
+
+    private void setWeeklyGlucoceText()
+    {
+
+        //Perform Database Operations in orderto get Daily Glucoze
+
+        TextView DailyTextView = (TextView) findViewById(R.id.txtWeekly);
+        //Set the Text for Weekly Text View
+
+
+
+    }
+
+    private void setMonthlyGlucoceText()
+    {
+
+        //Perform Database Operations in orderto get Daily Glucoze
+
+        TextView DailyTextView = (TextView) findViewById(R.id.txtMonthly);
+        //Set the Text for Monthly Text View
+
+
+
+    }
+
+
+
+}
